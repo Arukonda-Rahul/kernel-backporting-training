291156f295c21ddea262946b621dd18cff8702e4
Rahul <rahularukonda2@gmail.com>
2024-05-07 17:30:21 +0530

    hello.c

--- Module_without_exit/hello.c
+++ Module_without_exit/hello.c
@@ -8,6 +8,8 @@ static int test_init(void)
 	printk(KERN_INFO"hi\n");
 	return 0;
 }
-
+/* it will load kernel module but when u try to rmmod it it will display
+ * rmmod: ERROR: ../libkmod/libkmod-module.c:799 kmod_module_remove_module() could not remove 'hello': Device or resource busy
+rmmod: ERROR: could not remove module hello.ko: Device or resource busy  */
 
 module_init(test_init);
