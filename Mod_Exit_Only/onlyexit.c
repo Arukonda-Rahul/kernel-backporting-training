5ea28729a3532c1b42158fa213bb5ac119dfc24e
Rahul <rahularukonda2@gmail.com>
2024-05-07 17:35:38 +0530

    Only exit

--- Mod_Exit_Only/onlyexit.c
+++ Mod_Exit_Only/onlyexit.c
@@ -5,7 +5,10 @@ MODULE_LICENSE("GPL");
 
 static void test_exit(void)
 {
-	printk(KERN_INFO"Bye\n");
+	printk(KERN_INFO"Bye forever\n");
 }
-
+/*
+ * It is opposite for Module with only init 
+ * It will load module but no display as their is no Init You can remove and see the output in exit
+ * */
 module_exit(test_exit);
